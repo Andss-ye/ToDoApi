a0389159e4266849dc8c69d0f1d3d5f753a8fc92
Andss-ye <andrex120tv@gmail.com>
2024-11-14 07:10:50 -0500

    feat: :rocket: Se agrego la funcion de crear el todo en la bd en el modulo de modelos

--- models/todoService.js
+++ models/todoService.js
@@ -1,5 +1,6 @@
 import Todo from './todo.js'
 import fs from 'fs'
+import {v4 as uuidv4} from 'uuid'
 filePath = '../database/todos.json'
 
 class todoService {
@@ -8,7 +9,16 @@ class todoService {
         const todos = JSON.parse(data)
         return todos.map(todo => new Todo(todo.id, todo.title, todo.description, todo.complete))
     }
+
     saveData(todos){
         fs.writeFileSync(filePath, JSON.stringify(todos, null, 2))
+    }
+
+    createTodo(todos){
+        const todos = this.getData()
+        const newTodo = new Todo(uuidv4(), todos.title, todos.description, false)
+        todos.push(newTodo)
+        this.saveData(todos)
+        return newTodo
     }
 }
