95f8917dd30bb8a287aec034dad85732e648b358
Andss-ye <andrex120tv@gmail.com>
2024-11-16 01:54:20 -0500

    feat: :rocket: Se agregaron cosas para mejorar el manejo de archivos del json

--- models/todoService.js
+++ models/todoService.js
@@ -1,22 +1,26 @@
 import Todo from './todo.js'
 import fs from 'fs'
 import {v4 as uuidv4} from 'uuid'
-filePath = '../database/todos.json'
+import { fileURLToPath } from "url";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+let filePath = path.resolve(__dirname, '../database/todos.json')
 
 class todoService {
     getData(){
         const data = fs.readFileSync(filePath, 'utf-8')
         const todos = JSON.parse(data)
-        return todos.map(todo => new Todo(todo.id, todo.title, todo.description, todo.complete))
+        return todos.map(todo => new Todo(todo.id, todo.name, todo.description, todo.complete))
     }
 
     saveData(todos){
         fs.writeFileSync(filePath, JSON.stringify(todos, null, 2))
     }
 
-    createTodo(title, description){
+    createTodo(name, description){
         const todos = this.getData()
-        const newTodo = new Todo(uuidv4(), title, description, false)
+        const newTodo = new Todo(uuidv4(), name, description, false)
         todos.push(newTodo)
         this.saveData(todos)
         return newTodo
@@ -24,6 +28,7 @@ class todoService {
 
     updateTodo(data){
         const todos = this.getData()
+        const {id} = data
         const todoIndex = todos.findIndex(todo => todo.id === id)
         if(todoIndex === -1) throw new Error('Task not found in db')
         todos[todoIndex] = {
