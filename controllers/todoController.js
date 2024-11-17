693f628f168260589419d520713622ecdccadb50
Andss-ye <andrex120tv@gmail.com>
2024-11-17 01:16:51 -0500

    feat: :rocket: Se agrego la funcion de eliminacion de todos en el controller y rutas

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -29,3 +29,13 @@ export const updateTodo = async (req, res) => {
         res.status(500).send(error.message);   
     }
 }
+
+export const deleteTodo = async (req, res) => {
+    try {
+        const {id} = req.params
+        await todoService.deleteTodo(id)
+        res.status(200).json({message: 'Todo deleted'})
+    } catch (error) {
+        res.status(500).send(error.message);   
+    }
+}
